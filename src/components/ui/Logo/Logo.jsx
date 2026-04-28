2a20f490ea5e1ecc485029795b543dd6c98e469b
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-28 14:10:07 +0600

    refactor: change logo and text sizes

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -5,10 +5,10 @@ const Logo = () => {
 
     return (
         <div className="flex items-center justify-center gap-2 sm:gap-4">
-            <div className="relative w-8 h-8 transition-all duration-300 sm:w-10 sm:h-10 md:w-[50px] md:h-[50px]">
+            <div className="relative w-8 h-8 transition-all duration-300 sm:w-10 sm:h-10 md:w-[40px] md:h-[40px]">
                 <Image src={logo} alt="logo" fill className="object-cover w-full h-full"></Image>
             </div>
-            <span className="text-2xl font-bold tracking-[-2px] transition-all duration-300 sm:text-3xl md:text-4xl">SynthGallery</span>
+            <span className="text-2xl font-bold tracking-[-2px] transition-all duration-300 sm:text-3xl">SynthGallery</span>
         </div>
     );
 };
