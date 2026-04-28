44b15be165c7d8a842b38158ba5dba62bda4f012
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-28 14:07:41 +0600

    feat: add navlinks.js for use navlinks dynamic

---
+++ src/constants/navlinks.js
@@ -0,0 +1,17 @@
+export const navlinks = [
+    {
+        id: 1,
+        path: '/',
+        name: 'Home'
+    },
+    {
+        id: 2,
+        path: '/all-photos',
+        name: 'All Photos'
+    },
+    {
+        id: 3,
+        path: '/profile',
+        name: 'Profile'
+    },
+];
